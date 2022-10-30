4727938d728d9d11fdc8285727d08e3006a24675
William A. Morris <wm59@st-andrews.ac.uk>
2022-10-30 12:11:19 +0000

    Created ship movement

--- hub.js
+++ hub.js
@@ -1,14 +1,42 @@
 'use strict';
-function moveUF0X(direction){
-    let ufo = document.getElementsByClassName("ufo");
+
+document.onkeydown = detectKey;
+
+
+function detectKey(key){
+    switch(key.keyCode){
+        case 38:
+            //up arrow
+            moveShip(up);
+        case 40:
+            //down arrow
+            moveShip(down);
+        case 37:
+            //left arrow
+            moveShip(left);
+        case 39:
+            //right arrow  
+            moveShip(right);      
+    }
 }
 
-function moveUF0Y(direction){
-    let ufo = document.getElementsByClassName("ufo");
+function moveShip(direction){
+    let ship = document.getElementsByClassName("ship");
+    let positionY = (ship.style.top).replace("px","");
+    let positionX = (ship.style.left).replace("px","");
 
-    if(direction = 1){
-
-    }else{
-        ufo.style.position
+    switch(direction){
+        case "up":
+            ship.style.top = (positionY + 0.01) +"px";
+            break;
+        case "down":
+            ship.style.top = (positionY - 0.01) +"px";
+            break;
+        case "left":
+            ship.style.left = (positionX + 0.01) +"px";
+            break;
+        case "right":
+            ship.style.left = (positionX - 0.01) +"px";
+            break;
     }
 }
