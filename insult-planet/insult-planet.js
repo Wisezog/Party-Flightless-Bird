2c0f24f13d93e6fc1e269326b1a12c012e616c98
William A. Morris <wm59@st-andrews.ac.uk>
2022-10-30 17:12:12 +0000

    Added alien scripts

--- insult-planet/insult-planet.js
+++ insult-planet/insult-planet.js
@@ -1,13 +1,17 @@
-'use strict'
+'use strict';
 
 var currentIndex = 0; // Records the current/previous index to ensure the next is different.
 
 function insult(file) {
-    let insults = file.toString().split('\r\n')
+  
+    let insults = file.toString().split("\n")
 
     let index = Math.floor(Math.random() * insults.length);
+
     while (index == currentIndex) {
+        console.log(index);
         index = Math.floor(Math.random() * insults.length);
+   
     }
     currentIndex = index;
 
