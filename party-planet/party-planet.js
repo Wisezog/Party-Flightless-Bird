52fd730e1983e1dae46f1e10a7215d70d4876be8
James Hart <masterjameshart@yahoo.com.sg>
2022-10-30 17:55:22 +0000

    party planet

--- party-planet/party-planet.js
+++ party-planet/party-planet.js
@@ -3,20 +3,6 @@
 var currentIndex = 0; // Records the current/previous index to ensure the next is different.
 
 function loadParty(){
-
-    let index = Math.floor(Math.random() * insults.length);
-
-    const array = ['firstSong.mp3', ''];
-
-    while (index == currentIndex) {
-        index = Math.floor(Math.random() * array.length);
-    }
-    currentIndex = index;
-
-    commenceParty(array[index]);
-}
-
-function commenceParty(song){
-    var audio = new Audio(song);
+    var audio = new Audio('./halloween-spooky-trap.mp3');
     audio.play();
 }
