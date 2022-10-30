6f2e2455674c7c28de0c6e2d07ac1c5a6c3a8818
William A. Morris <wm59@st-andrews.ac.uk>
2022-10-30 19:38:59 +0000

    naughts and crosses

--- connect-4-planet/connect-4-planet.js
+++ connect-4-planet/connect-4-planet.js
@@ -1,44 +1,65 @@
 'use strict'
 
-var array = new Array[3][3];
+var array = [0,0,0,0,0,0,0,0,0];
+
 // 0 = empty
 // 1 = X (player)
 // 2 = O (Computer)
 var win = false;
 var error = false;
+var currentPlayer = 1;
 
 array.forEach(element => {
     element = 0;
 });
 
-function play(x, y) {
-    if (array[x][y] != 0) {
+function play(cell) {
+
+    if (array[cell] != 0) {
         error = true;
         callError();
     } else {
-        array[x][y] = 1;
+        array[cell] = currentPlayer;
+        let button = document.getElementById("cell"+cell);
+        button.value = currentPlayer;
+        checkWin(currentPlayer,cell);
+        if(currentPlayer == 1){
+            currentPlayer = 2;
+        }else{
+            currentPlayer = 1;
+        }
     }
-    checkWin();
+
 }
 
-function checkWin() {
+function checkWin(currentPlayer,cell) {
     for (let index = 0; index < array.length; index++) {
-        if (array[index][0] == 1 && array[index][1] == 1 && array[index][2] == 1){
-            win = true;
-        }
-        if (array[0][index] == 1 && array[1][index] == 1 && array[2][index] == 1){
-            win = true;
-        }
-        if (array[index][index] == 1 && array[index][index] == 1 && array[index][index] == 1){
-            win = true;
-        }
-        if (array[array.length- index][index] == 1 && array[index][index] == 1 && array[array.length][index] == 1){
-            win = true;
+        if(array[0] == currentPlayer && array[1] == currentPlayer && array[2] == currentPlayer){
+            win = true
+        }else if(array[3] == currentPlayer && array[4] == currentPlayer && array[5] == currentPlayer){
+            win = true
+        }else if(array[6] == currentPlayer && array[7] == currentPlayer && array[8] == currentPlayer){
+            win = true
+        }else if(array[0] == currentPlayer && array[3] == currentPlayer && array[6] == currentPlayer){
+            win = true
+        }else if(array[1] == currentPlayer && array[4] == currentPlayer && array[7] == currentPlayer){
+            win = true
+        }else if(array[2] == currentPlayer && array[5] == currentPlayer && array[6] == currentPlayer){
+            win = true
+        }else if(array[0] == currentPlayer && array[4] == currentPlayer && array[8] == currentPlayer){
+            win = true
+        }else if(array[2] == currentPlayer && array[4] == currentPlayer && array[6] == currentPlayer){
+            win = true
         }
     }
 
     if (win){
-
+        alert("Player " + currentPlayer + " wins");
+        array = [0,0,0,0,0,0,0,0,0];
+        for(let i = 0; i < 9; i++){
+            let button = document.getElementById("cell"+i);
+            button.value = 0;
+        }
     }
 }
 
