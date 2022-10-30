f1ad32aa661a6a2e8cfa245007cee44699ccc4ae
William A. Morris <wm59@st-andrews.ac.uk>
2022-10-30 11:45:05 +0000

    Started ship movement functionality

---
+++ hub.js
@@ -0,0 +1,14 @@
+'use strict';
+function moveUF0X(direction){
+    let ufo = document.getElementsByClassName("ufo");
+}
+
+function moveUF0Y(direction){
+    let ufo = document.getElementsByClassName("ufo");
+
+    if(direction = 1){
+
+    }else{
+        ufo.style.position
+    }
+}
