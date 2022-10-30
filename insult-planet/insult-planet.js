cf717883fab214dd83d20d38555a779866a40a30
William A. Morris <wm59@st-andrews.ac.uk>
2022-10-30 17:41:11 +0000

    format changes

--- insult-planet/insult-planet.js
+++ insult-planet/insult-planet.js
@@ -3,12 +3,8 @@
 var currentIndex = 0; // Records the current/previous index to ensure the next is different.
 
 function insult(file) {
-<<<<<<< HEAD
-    let insults = file.toString().split('\n')
-=======
   
     let insults = file.toString().split("\n")
->>>>>>> 19d5f1cf8ea48c10f74d8ffa2abef9ea78894a0a
 
     let index = Math.floor(Math.random() * insults.length);
 
@@ -21,3 +17,4 @@ function insult(file) {
 
     return insults[index];
 }
+
