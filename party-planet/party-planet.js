1be4bbe66a29d08145d93d27dedc9a4d24aa46ab
James Hart <masterjameshart@yahoo.com.sg>
2022-10-30 20:01:28 +0000

    final final

--- party-planet/party-planet.js
+++ party-planet/party-planet.js
@@ -1,7 +1,5 @@
 'use strict'
 
-var currentIndex = 0; // Records the current/previous index to ensure the next is different.
-
 function loadParty(){
     var audio = new Audio('./halloween-spooky-trap.mp3');
     audio.play();
